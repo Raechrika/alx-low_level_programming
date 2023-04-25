70f9bc2a7f522782702067b3dd702c0dab39544b
Raechrika <rachel.erica.gh@gmail.com>
2023-04-25 23:30:53 +0000

    print_putchar_commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+main-print_putchar
+*/
+int main() {
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return 0;
+}
