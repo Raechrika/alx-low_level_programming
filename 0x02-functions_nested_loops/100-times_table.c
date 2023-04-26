54e1be01c9c04caf28d3011935f49ad718e4dfe9
Raechrika <rachel.erica.gh@gmail.com>
2023-04-26 15:07:44 -0700

    times_table_commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,6 +17,7 @@ if (j == 0)
 {
 _putchar(k + '0');
 } else if (k < 10 && j != 0)
+}
 {
 {
 _putchar(',');
@@ -25,7 +26,8 @@ _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
 }
-}else if (k >= 10 && k < 100)
+} else if (k >= 10 && k < 100)
+}
 {
 _putchar(',');
 _putchar(' ');
@@ -33,6 +35,7 @@ _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 } else if (k >= 100)
+}
 {
 _putchar(',');
 _putchar(' ');
